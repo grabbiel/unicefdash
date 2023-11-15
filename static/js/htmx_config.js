ae4c45a21961cac95eb9d71ce3fbd59fead1bf4a
pakitow <90626364+pakitow@users.noreply.github.com>
2023-11-15 11:51:03 -0600

    modified js and html

--- static/js/htmx_config.js
+++ static/js/htmx_config.js
@@ -1,8 +1,26 @@
-document.body.addEventListener("htmx:validateURL", function (evt) {
-  if (
-    !evt.detail.sameHost &&
-    !evt.detail.url.hostname !== "raw.githubusercontent.com"
-  ) {
-    evt.PreventDefault();
-  }
-});
+/*
+window.onload = function () {
+  document.body.addEventListener("htmx:configRequest", function (evt) {
+    evt.detail.headers = "";
+    evt.detail.headers["Content-Type"] =
+      "application/x-www-form-urlencoded; charset=utf-8";
+  });
+
+  document.body.addEventListener("htmx:beforeRequest", function (evt) {
+    evt.detail.xhr.upload = "";
+  });
+
+  document.body.addEventListener("htmx:beforeSend", function (evt) {
+    evt.detail.xhr.upload = "";
+  });
+
+  document.body.addEventListener("htmx:validateURL", function (evt) {
+    if (
+      !evt.detail.sameHost &&
+      !evt.detail.url.hostname !== "raw.githubusercontent.com"
+    ) {
+      evt.PreventDefault();
+    }
+  });
+};
+*/
